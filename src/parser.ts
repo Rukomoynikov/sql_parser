a6d02177e047f1615c8d3137ef9464de2643e80a
Max Rukomoynikov <rukomoynikov@gmail.com>
2025-01-05 23:00:03 +0000

    Parse FROM

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,9 @@
 import { Token, TokenType } from "./token.ts";
 import { Query } from "./expressions/query.ts";
-import { SelectExpression, type SelectValue } from "./expressions/select_expression.ts";
+import {
+  SelectExpression,
+  type SelectValue,
+} from "./expressions/select_expression.ts";
 
 interface ParserParams {
   tokens: Token[];
@@ -15,7 +18,10 @@ class Parser {
   }
 
   parse(): Query {
-    return new Query({ select: this.select() });
+    return new Query({
+      select: this.select(),
+      from: this.from(),
+    });
   }
 
   select(): SelectExpression {
@@ -23,20 +29,38 @@ class Parser {
       this.tokens[this.current] && this.tokens[this.current].type;
 
     if (tokenType == TokenType.Select) {
-      this.current++
-      const value: SelectValue = []
+      this.current++;
+      const value: SelectValue = [];
 
-      while (this.tokens[this.current] && (this.tokens[this.current].type === TokenType.Identifier || this.tokens[this.current].type === TokenType.Star)) {
-        console.log(this.tokens[this.current])
-        value.push(this.tokens[this.current].content)
+      while (
+        this.tokens[this.current] &&
+        (this.tokens[this.current].type === TokenType.Identifier ||
+          this.tokens[this.current].type === TokenType.Star)
+      ) {
+        value.push(this.tokens[this.current].content);
 
-        this.current++
+        this.current++;
       }
-
-      this.current--
 
       return new SelectExpression(value);
     }
+
+    throw new Error("SELECT expresion is expected");
+  }
+
+  from(): string {
+    const tokenType =
+      this.tokens[this.current] && this.tokens[this.current].type;
+
+    if (tokenType == TokenType.From) {
+      this.current++;
+
+      if (this.tokens[this.current].type === TokenType.Identifier) {
+        return this.tokens[this.current].content;
+      }
+    }
+
+    throw new Error("FROM expresion is expected");
   }
 }
 
